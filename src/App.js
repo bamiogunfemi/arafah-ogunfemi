f65e8c614f40342c23f179a5d77eac2e2db2f0d7
bamiogunfemi <ayobamiarafat09@gmail.com>
2022-06-16 15:38:37 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [path, setPath] = useState(null);
+  const [err, setErr] = useState(null);
+  function findKey(obj, target) {
+    const fnd = (obj) => {
+      for (const [k, v] of Object.entries(obj.user)) {
+        if (typeof v === "object" && query) {
+          for (const [i, j] of Object.entries(v)) {
+            if (j.toLowerCase() === target?.toLowerCase()) {
+              setErr("");
+              return `${i}.${k}`;
+            } else {
+              setErr("Path does not exist");
+            }
+          }
+        } else {
+          if (String(v) === target && query) {
+            setErr("");
+            return k;
+          } else if (query) {
+            setErr("Path does not exist");
+          }
+        }
+      }
+    };
+    return fnd(obj);
+  }
+  const a = {
+    user: {
+      id: 1,
+      name: {
+        firstName: "James",
+        lastName: "Ibori",
+      },
+      location: {
+        city: "Ikoyi",
+        state: "Lagos",
+        address: "One expensive house like that",
+      },
+    },
+  };
+  useEffect(() => {
+    if (query !== "") {
+      setErr("");
+    }
+    setPath(findKey(a, query));
+  }, [query]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <div className="search">
+        <div className="icon"></div>
+        <div className="input">
+          <input
+            value={query}
+            type="text"
+            placeholder="Search..."
+            onChange={(e) => setQuery(e.target.value)}
+            id="mysearch"
+          />
+        </div>
+      </div>
+      {err && <h4 className="error">{err}</h4>}
+      {query !== "" && path && (
+        <h3>
+          Path is `pathGet(a, {query}) ="a.user.{path}"`
+        </h3>
+      )}
+    </section>
   );
 }
 
